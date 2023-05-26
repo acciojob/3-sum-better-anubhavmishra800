88f2ea08af40e1531a6c653ffc6e4927f0b0a181
anubhavmishra800 <127845673+anubhavmishra800@users.noreply.github.com>
2023-05-26 14:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  arr.sort((a,b) =>a-b);
+	let closetSum=infinity;
+	for(let i=0;i<arr.length-2;i++){
+		let left=i+1;
+		let right=arr.length-1;
+		while(left<right){
+			let sum=arr[i]+arr[left]+arr[right];
+			if(Math.abs(sum-target)<Math.abs(closetSum-target)){
+				closetSum=sum;
+			}
+			if(sum<target){
+				left++;
+			}
+			else if(sum>target){
+				right--;
+			}
+			else{
+				return closetSum;
+			}
+		}
+	}
+	return closetSum;
 }
 
 module.exports = threeSum;
+
